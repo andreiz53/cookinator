071ae6acd387858d1a4ea1003c3ebcdf3d5479ff
andreiz53 <andrei.zamfira53@gmail.com>
2025-01-31 16:42:42 +0200

    test get ingredients

--- server/handler_ingredient_test.go
+++ server/handler_ingredient_test.go
@@ -10,77 +10,78 @@ import (
 	database "github.com/andreiz53/cookinator/database/handlers"
 	databaseMock "github.com/andreiz53/cookinator/database/mocks"
 	"github.com/andreiz53/cookinator/util"
-	"github.com/gin-gonic/gin"
 	"github.com/jackc/pgx/v5"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 )
 
-func randomIngredient() database.Ingredient {
-	return database.Ingredient{
-		ID:      int32(util.RandomInt(1, 1000)),
-		Name:    util.RandomName(),
-		Density: util.RandomPGNumeric(),
-	}
-}
-
-func createRandomIngredient(t *testing.T) Ingredient {
-	params := database.CreateIngredientParams{
-		Name:    util.RandomName(),
-		Density: util.RandomPGNumeric(),
-	}
-
-	expectedValue := database.Ingredient{
-		ID:      1,
-		Name:    params.Name,
-		Density: params.Density,
-	}
-
-	store := new(databaseMock.MockStore)
-	server := NewServer(store)
-	gin.SetMode(gin.TestMode)
-	store.EXPECT().CreateIngredient(mock.Anything, params).Times(1).Return(expectedValue, nil)
-
-	recorder := httptest.NewRecorder()
-	url := "/ingredients"
-
-	data, err := encodeJSON(params)
-	require.NoError(t, err)
-	request, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(data))
-	require.NoError(t, err)
-	request.Header.Set("Content-Type", "application/json")
-
-	server.router.ServeHTTP(recorder, request)
-
-	require.Equal(t, http.StatusCreated, recorder.Code)
-	return requireBodyMatchIngredient(t, recorder.Body, expectedValue)
-}
-
-func requireBodyMatchIngredient(t *testing.T, body *bytes.Buffer, ingredient database.Ingredient) Ingredient {
-	createdIngredient, err := decodeJSON[Ingredient](body)
-	require.NoError(t, err)
-	require.NotEmpty(t, createdIngredient)
-
-	require.Equal(t, ingredient.Name, createdIngredient.Name)
-	require.Equal(t, ingredient.Density, createdIngredient.Density)
-
-	return createdIngredient
-}
-
 func TestCreateIngredient(t *testing.T) {
-	createRandomIngredient(t)
+
 }
 
 func TestGetIngredients(t *testing.T) {
+	var ingredients []database.Ingredient
+	n := 3
+	for i := 0; i < n; i++ {
+		ingredients = append(ingredients, randomIngredient())
+	}
 
+	testCases := []struct {
+		name          string
+		ingredients   []database.Ingredient
+		stubs         func(store *databaseMock.MockStore)
+		checkResponse func(t *testing.T, recorder *httptest.ResponseRecorder)
+	}{
+		{
+			name:        "OK",
+			ingredients: ingredients,
+			stubs: func(store *databaseMock.MockStore) {
+				store.EXPECT().
+					GetIngredients(mock.Anything).
+					Times(1).
+					Return(ingredients, nil)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusOK, recorder.Code)
+				requireBodyMatchIngredients(t, recorder.Body, ingredients)
+			},
+		},
+		{
+			name:        "InternalServerError",
+			ingredients: []database.Ingredient{},
+			stubs: func(store *databaseMock.MockStore) {
+				store.EXPECT().
+					GetIngredients(mock.Anything).
+					Times(1).
+					Return([]database.Ingredient{}, pgx.ErrTxClosed)
+			},
+			checkResponse: func(t *testing.T, recorder *httptest.ResponseRecorder) {
+				require.Equal(t, http.StatusInternalServerError, recorder.Code)
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			store := new(databaseMock.MockStore)
+			server := NewServer(store)
+
+			tc.stubs(store)
+
+			recorder := httptest.NewRecorder()
+			url := "/ingredients"
+
+			request, err := http.NewRequest(http.MethodGet, url, nil)
+			require.NoError(t, err)
+
+			server.router.ServeHTTP(recorder, request)
+			tc.checkResponse(t, recorder)
+		})
+	}
 }
 
 func TestGetIngredientByID(t *testing.T) {
-	ingredient := database.Ingredient{
-		ID:      int32(util.RandomInt(100, 10000)),
-		Name:    util.RandomName(),
-		Density: util.RandomPGNumeric(),
-	}
+	ingredient := randomIngredient()
 
 	testCases := []struct {
 		name          string
@@ -165,3 +166,65 @@ func TestGetIngredientByID(t *testing.T) {
 func TestUpdateIngredient(t *testing.T) {}
 
 func TestDeleteIngredient(t *testing.T) {}
+
+func randomIngredient() database.Ingredient {
+	return database.Ingredient{
+		ID:      int32(util.RandomInt(1, 1000)),
+		Name:    util.RandomName(),
+		Density: util.RandomPGNumeric(),
+	}
+}
+
+// func createRandomIngredient(t *testing.T) Ingredient {
+// 	params := database.CreateIngredientParams{
+// 		Name:    util.RandomName(),
+// 		Density: util.RandomPGNumeric(),
+// 	}
+
+// 	expectedValue := database.Ingredient{
+// 		ID:      1,
+// 		Name:    params.Name,
+// 		Density: params.Density,
+// 	}
+
+// 	store := new(databaseMock.MockStore)
+// 	server := NewServer(store)
+// 	gin.SetMode(gin.TestMode)
+// 	store.EXPECT().CreateIngredient(mock.Anything, params).Times(1).Return(expectedValue, nil)
+
+// 	recorder := httptest.NewRecorder()
+// 	url := "/ingredients"
+
+// 	data, err := encodeJSON(params)
+// 	require.NoError(t, err)
+// 	request, err := http.NewRequest(http.MethodPost, url, bytes.NewReader(data))
+// 	require.NoError(t, err)
+// 	request.Header.Set("Content-Type", "application/json")
+
+// 	server.router.ServeHTTP(recorder, request)
+
+// 	require.Equal(t, http.StatusCreated, recorder.Code)
+// 	return requireBodyMatchIngredient(t, recorder.Body, expectedValue)
+// }
+
+func requireBodyMatchIngredient(t *testing.T, body *bytes.Buffer, ingredient database.Ingredient) {
+	createdIngredient, err := decodeJSON[Ingredient](body)
+	require.NoError(t, err)
+	require.NotEmpty(t, createdIngredient)
+
+	require.Equal(t, ingredient.Name, createdIngredient.Name)
+	require.Equal(t, ingredient.Density, createdIngredient.Density)
+}
+
+func requireBodyMatchIngredients(t *testing.T, body *bytes.Buffer, ingredients []database.Ingredient) {
+	createdIngredients, err := decodeJSON[[]Ingredient](body)
+	require.NoError(t, err)
+	require.NotEmpty(t, createdIngredients)
+
+	require.Equal(t, len(ingredients), len(createdIngredients))
+	for i, ingredient := range createdIngredients {
+		require.Equal(t, ingredient.Name, ingredients[i].Name)
+		require.Equal(t, ingredient.Density, ingredients[i].Density)
+		require.Equal(t, ingredient.ID, ingredients[i].ID)
+	}
+}
